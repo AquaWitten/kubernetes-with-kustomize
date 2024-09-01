272ce348fc9562e52f6168f8cb2cffc3e7b97fb6
Emil Wittendorff <emilwittendorff@hotmail.com>
2024-09-01 23:04:15 +0200

    Added base structure to the project

---
+++ api-service/src/main/groovy/com/aquawitten/api_service/ApiServiceApplication.groovy
@@ -0,0 +1,13 @@
+package com.aquawitten.api_service
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class ApiServiceApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(ApiServiceApplication, args)
+	}
+
+}
---
+++ frontend/frontend-service/src/main/groovy/com/aquawitten/frontend_service/FrontendServiceApplication.groovy
@@ -0,0 +1,13 @@
+package com.aquawitten.frontend_service
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class FrontendServiceApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(FrontendServiceApplication, args)
+	}
+
+}
