8ad52a2a0278662faad1e8b931d07a9ca38a827a
Emil Wittendorff <emilwittendorff@hotmail.com>
2024-09-02 23:25:42 +0200

    Removed Vue boiler plate code and styling

--- frontend/simple-notes/src/App.vue
+++ frontend/simple-notes/src/App.vue
@@ -1,47 +1,21 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import SimpleInput from './components/SimpleInput.vue'
 </script>
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
+    <h1>Simple Notes</h1>
+    <p>Enter your notes here to store them</p>
+    <SimpleInput msg="Input you text here to save it" />
   </header>
 
-  <main>
-    <TheWelcome />
-  </main>
+  <main></main>
 </template>
 
-<style scoped>
+<style scoped lang="scss">
 header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
+  h1 {
+    color: orange;
   }
 }
 </style>
---
+++ frontend/simple-notes/src/components/SimpleInput.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+defineProps<{
+  msg: string
+}>()
+</script>
+
+<template>
+  <h2>Make an input</h2>
+  <p>{{ msg }}</p>
+  <input type="text" />
+  <button>Submit Note</button>
+</template>
+
+<style scoped lang="scss"></style>
